dfa6c4caea348ab296d23c7cd327895d9655493d
Adrian Barglazan <jm_aba@yahoo.com>
2023-01-28 20:41:27 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,196 @@
+class Node(object):
+    def __init__(self, parent=None, left=None, right=None, weight=1, symbol=''):
+        super(Node, self).__init__()
+        self._parent = parent
+        self._left = left
+        self._right = right
+        self._weight = weight
+        self._symbol = symbol
+
+    def key(self):
+        return str(self.symbol) + " " + str(self.weight)
+
+    def display(self):
+        lines, *_ = self._display_aux()
+        for line in lines:
+            print(line)
+
+    def _display_aux(self):
+        """Returns list of strings, width, height, and horizontal coordinate of the root."""
+        # No child.
+        if self.right is None and self.left is None:
+            line = '%s' % self.key()
+            width = len(line)
+            height = 1
+            middle = width // 2
+            return [line], width, height, middle
+
+        # Only left child.
+        if self.right is None:
+            lines, n, p, x = self.left._display_aux()
+            s = '%s' % self.key()
+            u = len(s)
+            first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s
+            second_line = x * ' ' + '/' + (n - x - 1 + u) * ' '
+            shifted_lines = [line + u * ' ' for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, n + u // 2
+
+        # Only right child.
+        if self.left is None:
+            lines, n, p, x = self.right._display_aux()
+            s = '%s' % self.key()
+            u = len(s)
+            first_line = s + x * '_' + (n - x) * ' '
+            second_line = (u + x) * ' ' + '\\' + (n - x - 1) * ' '
+            shifted_lines = [u * ' ' + line for line in lines]
+            return [first_line, second_line] + shifted_lines, n + u, p + 2, u // 2
+
+        # Two children.
+        left, n, p, x = self.left._display_aux()
+        right, m, q, y = self.right._display_aux()
+        s = '%s' % self.key()
+        u = len(s)
+        first_line = (x + 1) * ' ' + (n - x - 1) * '_' + s + y * '_' + (m - y) * ' '
+        second_line = x * ' ' + '/' + (n - x - 1 + u + y) * ' ' + '\\' + (m - y - 1) * ' '
+        if p < q:
+            left += [n * ' '] * (q - p)
+        elif q < p:
+            right += [m * ' '] * (p - q)
+        zipped_lines = zip(left, right)
+        lines = [first_line, second_line] + [a + u * ' ' + b for a, b in zipped_lines]
+        return lines, n + m + u, max(p, q) + 2, n + u // 2
+
+    @property
+    def parent(self):
+        return self._parent
+
+    @parent.setter
+    def parent(self, parent):
+        self._parent = parent
+
+    @property
+    def left(self):
+        return self._left
+
+    @left.setter
+    def left(self, left):
+        self._left = left
+
+    @property
+    def right(self):
+        return self._right
+
+    @right.setter
+    def right(self, right):
+        self._right = right
+
+    @property
+    def weight(self):
+        return self._weight
+
+    @weight.setter
+    def weight(self, weight):
+        self._weight = weight
+
+    @property
+    def symbol(self):
+        return self._symbol
+
+    @symbol.setter
+    def symbol(self, symbol):
+        self._symbol = symbol
+
+class FGK(object):
+    def __init__(self):
+        super(FGK, self).__init__()
+        self.NYT = Node(symbol="NYT")
+        self.root = self.NYT
+        self.nodes = []
+        self.seen = [None] * 256
+
+    def get_code(self, s, node, code=''):
+        if node.left is None and node.right is None:
+            return code if node.symbol == s else ''
+        else:
+            temp = ''
+            if node.left is not None:
+                temp = self.get_code(s, node.left, code+'0')
+            if not temp and node.right is not None:
+                temp = self.get_code(s, node.right, code+'1')
+            return temp
+
+    def find_largest_node(self, weight):
+        for n in reversed(self.nodes):
+            if n.weight == weight:
+                return n
+
+    def swap_node(self, n1, n2):
+        i1, i2 = self.nodes.index(n1), self.nodes.index(n2)
+        self.nodes[i1], self.nodes[i2] = self.nodes[i2], self.nodes[i1]
+
+        tmp_parent = n1.parent
+        n1.parent = n2.parent
+        n2.parent = tmp_parent
+
+        if n1.parent.left is n2:
+            n1.parent.left = n1
+        else:
+            n1.parent.right = n1
+
+        if n2.parent.left is n1:
+            n2.parent.left = n2
+        else:
+            n2.parent.right = n2
+
+    def insert(self, s):
+        node = self.seen[ord(s)]
+
+        if node is None:
+            spawn = Node(symbol=s, weight=1)
+            internal = Node(symbol='', weight=2, parent=self.NYT.parent,
+                left=self.NYT, right=spawn)
+            spawn.parent = internal
+            self.NYT.parent = internal
+
+            if internal.parent is not None:
+                internal.parent.left = internal
+            else:
+                self.root = internal
+
+            self.nodes.insert(0, internal)
+            self.nodes.insert(0, spawn)
+
+            self.seen[ord(s)] = spawn
+            node = internal.parent
+
+        while node is not None:
+            largest = self.find_largest_node(node.weight)
+
+            if (node is not largest and node is not largest.parent and
+                largest is not node.parent):
+                self.swap_node(node, largest)
+
+            node.weight = node.weight + 1
+            node = node.parent
+        print("Printing tree")
+        self.root.display()
+    
+    def encode(self, text):
+        result = ''
+
+        for s in text:
+            if self.seen[ord(s)]:
+                result += " " + s + "=" + self.get_code(s, self.root)
+            else:
+                result += " NYT=" + self.get_code('NYT', self.root)
+                result +=  " "+ s + "=" + bin(ord(s))[2:].zfill(8)
+
+            self.insert(s)
+
+        return result
+
+    def get_symbol_by_ascii(self, bin_str):
+        return chr(int(bin_str, 2))
+
+text = "BBBCCCCCAA"
+print (FGK().encode(text))
